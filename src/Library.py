c93186638d5a8e21d8c5af3ca608a001f5098480
Zouhair El hadiq <zouhairelhadiq@gmail.com>
2023-02-11 17:10:17 +0100

    compute beta: done

--- src/Library.py
+++ src/Library.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import os
+import json
 
 
 def load_indexes(pathdir):
@@ -16,4 +17,14 @@ def load_indexes(pathdir):
         dfDictist[filename]=pd.read_csv(file)
     return dfDictist,indexes_names
 
+betaFunction=lambda i_index,j_index,Z,tho:np.exp(-abs(Z[i_index]-Z[j_index])/tho)
 
+def compute_beta(timezonePath,indexes_names,tho=3):
+    n=len(indexes_names)
+    beta=np.zeros((n,n))
+    with open(timezonePath) as file:
+        Z=json.load(file)["timezone"]
+    for i,i_index in enumerate(indexes_names):
+        for j,j_index in enumerate(indexes_names):
+            beta[i,j]=betaFunction(i_index,j_index,Z,tho)
+    return beta
